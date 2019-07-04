cb52b1ddc7ff31bdae9ab439ca270148e716e428
barnswills <barneywilliams90@hotmail.co.uk>
2019-07-04 12:11:24 +0100

    gfhgfhgfh

--- src/components/Interface.vue
+++ src/components/Interface.vue
@@ -1,21 +1,23 @@
 <template>
-<div>
-  <h1>Select a Type...</h1>
-  <TypeSelector/>
-</div>
+  <div>
+    <h1>Select a Type...</h1>
+    <TypeSelector/>
+    <PokemonHolder/>
+  </div>
 </template>
 
 <script>
+import TypeSelector from "./TypeSelector";
+import PokemonHolder from "./PokemonHolder";
 
-import TypeSelector from "./TypeSelector"
 export default {
-
   name: "Interface",
 
   components: {
-    TypeSelector
+    TypeSelector,
+    PokemonHolder
   }
-}
+};
 </script>
 
 <style></style>
---
+++ src/components/PokemonHolder.vue
@@ -0,0 +1,32 @@
+<template>
+  <sui-segments>
+    <sui-segment v-for="i in this.items" v-bind:key="i">{{ i }}</sui-segment>
+  </sui-segments>
+</template>
+
+<script>
+export default {
+  name: "PokemonHolder",
+
+  data() {
+    return { items: this.getItems(), 
+    dummy: ["hello", "world", "2019"] };
+  },
+
+  methods: {
+    getItems: async function() {
+      this.items = await this.$store.getters.selectedPokemon;
+      return await this.items;
+    }
+  },
+
+  watch: {
+    items: async function(val) {
+      console.log("ITEMS CHANGED!");
+
+      const v = await val;
+      console.log("CHANGED TO: ", v);
+    }
+  }
+};
+</script>
